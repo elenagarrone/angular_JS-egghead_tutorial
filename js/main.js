619ffcab3585b8a276e3c5ade28686859e2d8389
elena <elena.15@hotmail.it>
2015-01-20 17:33:53 +0000

    sharing data between controllers

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-function FirstCtrl($scope){
+// This is how you share data between two controllers: you create a service using the factory, you return whatever object you want to use (APIs). Once you've defined a service, the data can be injected in the controllers.
 
+var myApp = angular.module('myApp', []);
+
+myApp.factory('Data', function () {
+  return { message: "I'm data from a service" };
+});
+
+
+// We inject the 'Data into the first controller'
+function FirstCtrl($scope, Data){
+  $scope.data = Data;
 }
 
-function SecondCtrl($scope){
-
+function SecondCtrl($scope, Data){
+  $scope.data = Data;
 }
