486964c3da44678a0455a146f74bdb4b5767680e
Elena <elena.15@hotmail.it>
2015-01-21 13:39:44 +0000

    directive using class linked to a function

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ var app = angular.module('superhero', [])
 
 app.directive('superman', function(){
   return {
-    restrict: "A",
-    link: function(){ //linking function to the A (attribute)
+    restrict: "C", //stands for class
+    link: function(){
       alert("I'm working")
     }
   }
