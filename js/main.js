6839d3496b4731ee1d506f0e8c084291adac8f43
Elena <elena.15@hotmail.it>
2015-01-21 13:42:05 +0000

    directive using comment linked to a function

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var app = angular.module('superhero', [])
 
 app.directive('superman', function(){
   return {
-    restrict: "C", //stands for class
+    restrict: "M", //stands for comment but it's barely used
     link: function(){
       alert("I'm working")
     }
