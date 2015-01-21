a7c28c523db2b76768604824cd5758e55fa070cf
Elena <elena.15@hotmail.it>
2015-01-21 14:04:09 +0000

    useful behaviors

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
 var app = angular.module('behaviorApp', [])
 
-//Because putting restrict 'A' and 'link' is such a common user case, we can just write like this:
 app.directive('enter', function(){
-  return function(scope, element){
+  return function(scope, element, attribute){
     element.bind('mouseenter', function(){
-      console.log("I'm inside of you!");
+      element.addClass(attribute.enter); //this refer to the html
     })
   }
 })
 
 app.directive('leave', function(){
-  return function(scope, element){
+  return function(scope, element, attribute){
     element.bind('mouseleave', function(){
-      console.log("I'm leaving on a jet plane!");
+      element.removeClass(attribute.enter);
     })
   }
 })
